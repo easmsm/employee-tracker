4be64ba7769c6847a13999721def18c2fe30d3d1
ekukwa <easmsm@gmail.com>
2022-08-16 22:10:32 -0400

    added case functions depending on what start menu list item was selected, planned out functions in the notes

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ USE company;
 
 CREATE TABLE department (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    deparment_name VARCHAR(30) NOT NULL
+    department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
